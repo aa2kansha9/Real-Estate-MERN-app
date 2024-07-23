3dba9689c02c9f5b246f24d563c05c684eb230a9
Aakansha Parab <aakansha.aparab@gmail.com>
2024-07-23 06:32:21 +0530

    Create update user API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,33 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs';
+
 export const test = (req, res) =>{
     res.json({
         message: 'hello aakansha',
     });
+}
+
+export const updateUser = async (req, res, next) =>{
+    if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only update your own account!'))
+    try {
+        if(req.body.password){
+            req.body.password = bcryptjs.hashSync(req.body.password, 10)
+        }
+
+        const updateUser = await User.findByIdAndUpdate(req.params.id,{
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password:req.body.password,
+                avatar: req.body.avatar,
+            }
+        }, {new: true})
+
+        const {password, ...rest} = updateUser._doc;
+
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error)
+    }
 }
