d55448b3abc9da62a6ca1f73d042cc5d44cf16d2
Aakansha Parab <aakansha.aparab@gmail.com>
2024-08-08 19:03:59 +0530

    Complete Project

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -49,13 +49,13 @@ export default function Home() {
     <div>
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
-        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+        <h1 className='text-blue-900 font-bold text-3xl lg:text-6xl'>
+          Find your next <span className='text-blue-400'>perfect</span>
           <br />
           place with ease
         </h1>
-        <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+        <div className='text-green-700 text-xs sm:text-sm'>
+          Oma Maa is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
